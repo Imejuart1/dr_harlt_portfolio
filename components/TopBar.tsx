b31d55ef5be3b742fa048362e93f65f81ca231e6
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2024-08-10 17:41:49 +0100

    adding the header

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,21 +1,24 @@
 // src/components/TopBar.tsx
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faLinkedinIn, faPinterestP, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import styles from './styles/TopBar.module.scss';
 
 const TopBar: React.FC = () => {
   return (
     <div className={styles.topBar}>
       <div className={styles.contactInfo}>
-        <span><i className="far fa-clock"></i> Mon - Sun 0900 - 2100</span>
-        <span><i className="fas fa-phone"></i> +1-2345-3455-33</span>
-        <span><i className="far fa-envelope"></i> test@gmail.com</span>
+        <span><FontAwesomeIcon icon={faClock} /> Mon - Sun 0900 - 2100</span>
+        <span><FontAwesomeIcon icon={faPhone} /> +1-2345-3455-33</span>
+        <span><FontAwesomeIcon icon={faEnvelope} /> test@gmail.com</span>
       </div>
       <div className={styles.socialMedia}>
-        <a href="#"><i className="fab fa-facebook-f"></i></a>
-        <a href="#"><i className="fab fa-linkedin-in"></i></a>
-        <a href="#"><i className="fab fa-pinterest-p"></i></a>
-        <a href="#"><i className="fab fa-twitter"></i></a>
-        <a href="#"><i className="fab fa-instagram"></i></a>
+        <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+        <a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+        <a href="#"><FontAwesomeIcon icon={faPinterestP} /></a>
+        <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+        <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
       </div>
     </div>
   );
