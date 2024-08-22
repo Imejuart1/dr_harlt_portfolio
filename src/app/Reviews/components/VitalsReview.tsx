accd78b07b1849c051f002d97ca9445afbdc5b5b
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2024-08-22 17:50:05 +0100

    reviews

---
+++ src/app/Reviews/components/VitalsReview.tsx
@@ -0,0 +1,255 @@
+// components/VitalsReview.tsx
+"use client";
+import React, { useState, useEffect } from 'react';
+import { FaStar, FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import styles from '../Review.module.scss';
+
+interface Slide {
+  text: string;
+  author: string;
+  date: string;
+  rating: number;
+}
+
+const vitalsSlides: Slide[] = [
+  {
+    text: "An amazing doctor who listens and explains everything thoroughly. Highly recommended.",
+    author: "John Doe",
+    date: "Dec 15, 2023",
+    rating: 4
+  },
+  {
+    text: "Dr Härtl is extremely knowledgeable and caring. I felt at ease throughout my treatment.",
+    author: "Jane Smith",
+    date: "Nov 20, 2023",
+    rating: 5
+  }
+];
+
+const VitalsReview: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [subTab, setSubTab] = useState<"overview" | "detailed">("overview");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % vitalsSlides.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const renderStars = (rating: number) => (
+    <>
+      {Array.from({ length: rating }, (_, i) => (
+        <FaStar key={i} className={styles.star} />
+      ))}
+      {Array.from({ length: 5 - rating }, (_, i) => (
+        <FaStar key={i + rating} className={styles.starEmpty} />
+      ))}
+    </>
+  );
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className={styles.reviewContainer}>
+      <div className={styles.reviewHeader}>
+        <img src='img/vitals.svg' alt="Vitals Icon" className={styles.reviewIcon2} />
+        <h2 className={styles.h2}>Vitals Reviews</h2>
+        <a href="https://www.vitals.com/doctors/Dr_Roger_Hartl.html" target="_blank" rel="noopener noreferrer" className={styles.moreLinkvitals}>See Source</a>
+      </div>
+      <p className={styles.choiceProvider}>Dr. Härtl is a Vitals Choice provider in Neurosurgery.</p>
+      <div className={styles.slideshowContainer}>
+        <div
+          className={styles.slideshow}
+          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        >
+          {vitalsSlides.map((slide, index) => (
+            <div className={styles.slide} key={index}>
+              <div className={styles.rating}>{renderStars(slide.rating)}</div>
+              <p className={styles.text}>{slide.text}</p>
+              <p className={styles.author}>- {slide.author}, {slide.date}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.dropdown} >
+            <div className={styles.dropdownHeader} onClick={toggleDropdown}>
+              <h3 className={styles.h3v}><strong>SEE VITALS FEEDBACK CHART</strong></h3>
+              {isDropdownOpen ? <FaChevronUp /> : <FaChevronDown />}
+            </div>
+            {isDropdownOpen && (
+              <>
+              <div className={styles.tabNavigation}>
+               <button
+                 className={`${styles.tabButton} ${subTab === "overview" ? styles.active : ""}`}
+                 onClick={() => setSubTab("overview")}
+               >
+                 Feedback Overview
+               </button>
+               <button
+                 className={`${styles.tabButton} ${subTab === "detailed" ? styles.active : ""}`}
+                 onClick={() => setSubTab("detailed")}
+               >
+                 Detailed Feedback
+               </button>
+               <div
+                 className={`${styles.tabUnderline} ${subTab === "overview" ? styles.left : styles.right}`}
+               />
+             </div>
+               {subTab === "overview" && (<div className={styles.feedbackOverview}>
+                <h2>Dr. Hartl's Reviews</h2>
+                <div className={styles.ratingSummary}>
+                  <div className={styles.ratingOverall}>
+                    <h3 className={styles.rateh3}>4.8</h3>
+                    <div className={styles.stariop}>
+                    <div className={styles.stars}>
+                      {renderStars(5)}
+                    </div>
+                    <p className={styles.ratep}> 306+ ratings, 214+ with a written review</p>
+                    </div>
+                  </div>
+                  <div className={styles.ratingBreakdown}>
+                    <div className={styles.ratingRow}>
+                      <span>5 Star</span>
+                      <div className={styles.ratingBar}>
+                        <div className={styles.ratingFill} style={{ width: '92%' }} />
+                      </div>
+                      <span>92%</span>
+                    </div>
+                    <div className={styles.ratingRow}>
+                      <span>4 Star</span>
+                      <div className={styles.ratingBar}>
+                        <div className={styles.ratingFill} style={{ width: '3%' }} />
+                      </div>
+                      <span>3%</span>
+                    </div>
+                    <div className={styles.ratingRow}>
+                      <span>3 Star</span>
+                      <div className={styles.ratingBar}>
+                        <div className={styles.ratingFill} style={{ width: '1%' }} />
+                      </div>
+                      <span>1%</span>
+                    </div>
+                    <div className={styles.ratingRow}>
+                      <span>2 Star</span>
+                      <div className={styles.ratingBar}>
+                        <div className={styles.ratingFill} style={{ width: '1%' }} />
+                      </div>
+                      <span>1%</span>
+                    </div>
+                    <div className={styles.ratingRow}>
+                      <span>1 Star</span>
+                      <div className={styles.ratingBar}>
+                        <div className={styles.ratingFill} style={{ width: '4%' }} />
+                      </div>
+                      <span>4%</span>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+            )}
+              {subTab === "detailed" && (<div className={styles.dropdownContent}>
+                <h4 className={styles.h4v}>Provider Feedback</h4>
+                <p className={styles.p}>96% positive</p>
+                <ul className={styles.ul}>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>Trusted the provider's decisions</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '96%' }} // Dynamically adjust width here
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>268 agree, 11 disagree</span>
+                    </div>
+                  </li>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>Listened & answered questions</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '95%' }} // Adjust according to the data
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>263 agree, 13 disagree</span>
+                    </div>
+                  </li>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>Explained conditions well</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '96%' }} // Adjust according to the data
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>263 agree, 11 disagree</span>
+                    </div>
+                  </li>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>Appointment wasn't rushed</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '96%' }} // Adjust according to the data
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>254 agree, 10 disagree</span>
+                    </div>
+                  </li>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>I felt respected</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '100%' }} // Adjust according to the data
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>21 agree, 0 disagree</span>
+                    </div>
+                  </li>
+                </ul>
+                <h4 className={styles.office}>Office + Staff Feedback</h4>
+                <p className={styles.p}>94% positive</p>
+                <ul className={styles.ul}>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>Staff friendliness</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '96%' }} // Adjust according to the data
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>252 agree, 10 disagree</span>
+                    </div>
+                  </li>
+                  <li className={styles.li}>
+                    <span className={styles.feedbackText}>Appointment scheduling</span>
+                    <div className={styles.feedbackBar}>
+                      <div className={styles.progress}>
+                        <div
+                          className={styles.progressFilled}
+                          style={{ width: '92%' }} // Adjust according to the data
+                        ></div>
+                      </div>
+                      <span className={styles.feedbackVotes}>231 agree, 21 disagree</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>)}
+              </> )}
+          </div>
+        </div>
+  );
+};
+
+export default VitalsReview;
--- src/app/Reviews/page.tsx
+++ src/app/Reviews/page.tsx
@@ -1,73 +1,68 @@
-"use client"
-import React, { useState } from 'react';
+// app/reviews/page.tsx
+"use client";
+import React, { useEffect, useState } from 'react';
+import HealthgradesReview from './components/HealthgradesReview';
+import VitalsReview from './components/VitalsReview';
 import styles from './Review.module.scss';
 
-interface Slide {
-  image: string;
-  text: string;
-  link: string;
-}
-
-const slides: Slide[] = [
-  {
-    image: 'img/review1.jpg',
-    text: '"Dr. Härtl provided exceptional care during my spinal surgery..."',
-    link: 'https://example.com/full-review-1'
-  },
-  {
-    image: 'img/review2.jpg',
-    text: '"I am grateful for the outstanding care and expertise..."',
-    link: 'https://example.com/full-review-2'
-  },
-  {
-    image: 'img/review3.jpg',
-    text: '"The best spinal surgeon I have ever encountered..."',
-    link: 'https://example.com/full-review-3'
-  }
+const images = [
+  'img/reviewmain1.jpeg',
+  'img/reviewmain3.jpeg'
 ];
 
-const Review: React.FC = () => {
-  const [currentSlide, setCurrentSlide] = useState<number>(0);
+const ReviewPage: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<"reviews" | "gratitude">("reviews");
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const nextSlide = () => {
-    setCurrentSlide((prev) => (prev + 1) % slides.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
-  };
+  useEffect( () => {
+   const interval = setInterval(() =>{
+    setCurrentImageIndex((prevIndex)=> (prevIndex + 1) % images.length)
+   }, 5000);
+   return () => clearInterval(interval);
+  })
 
   return (
     <section className={styles.reviewSection}>
-      <div className={styles.heroSection} style={{ backgroundImage: `url('img/review-background.jpg')` }}>
-        <div className={styles.content_section}>
+      <div className={styles.heroSection} style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
+      <div className={styles.content}>
           <h1>Reviews & Gratitude</h1>
-          <p>Read what our patients have to say about their experiences.</p>
+          <p>Read what <strong>Dr.Harlts</strong> patients have to say about their experiences.</p>
         </div>
       </div>
-
-      <div className={styles.slideshow}>
-        <div className={styles.slide} style={{ backgroundImage: `url(${slides[currentSlide].image})` }}>
-          <div className={styles.reviewText}>
-            <p>{slides[currentSlide].text}</p>
-            <a href={slides[currentSlide].link} target="_blank" rel="noopener noreferrer">Read more</a>
+      <div className={styles.tabNavigation}>
+        <button
+          className={`${styles.tabButton} ${activeTab === "reviews" ? styles.active : ""}`}
+          onClick={() => setActiveTab("reviews")}
+        >
+          Reviews
+        </button>
+        <button
+          className={`${styles.tabButton} ${activeTab === "gratitude" ? styles.active : ""}`}
+          onClick={() => setActiveTab("gratitude")}
+        >
+          Gratitude
+        </button>
+        <div
+          className={`${styles.tabUnderline} ${activeTab === "reviews" ? styles.left : styles.right}`}
+        />
+      </div>
+      {activeTab === "reviews" && (
+        <div className={styles.reviewSection_content}>
+          <div id="healthgrades" className={styles.healthgSection}>
+            <HealthgradesReview />
+          </div>
+          <div id="vitals" className={styles.vitalsSection}>
+            <VitalsReview />
           </div>
         </div>
-        <button onClick={prevSlide} className={styles.prevButton}>‹</button>
-        <button onClick={nextSlide} className={styles.nextButton}>›</button>
-      </div>
-
-      <div className={styles.reviewLinks}>
-        <h2>Read Full Reviews</h2>
-        <ul>
-          <li><a href="https://example.com/review-google" target="_blank" rel="noopener noreferrer">Google Review for Och Spine at NewYork-Presbyterian</a></li>
-          <li><a href="https://example.com/review-vitals" target="_blank" rel="noopener noreferrer">Vitals</a></li>
-          <li><a href="https://example.com/review-healthgrades" target="_blank" rel="noopener noreferrer">Healthgrades</a></li>
-          <li><a href="https://example.com/gratitude" target="_blank" rel="noopener noreferrer">Learn more about how you can show your gratitude for Dr. Härtl and the team.</a></li>
-        </ul>
-      </div>
+      )}
+      {activeTab === "gratitude" && (
+        <div>
+          {/* Gratitude content goes here */}
+        </div>
+      )}
     </section>
   );
 };
 
-export default Review;
+export default ReviewPage;
