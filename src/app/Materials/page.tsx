9effe551e7ec0774c1e497b839bc04cc4815bb97
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2025-11-14 19:51:59 +0100

    adjust page numebr

--- src/app/Materials/page.tsx
+++ src/app/Materials/page.tsx
@@ -47,9 +47,8 @@ const MaterialComponent: React.FC = () => {
           <ul>
              <li><Link href="/pdfs/SpineHealthie Flyer.pdf"  target='_blank'>Spine Healthie - An App to Track Progress</Link></li>
             <li><Link href="/pdfs/MSC Information Sheet Draft 1.pdf"  target='_blank'>Lumbar Disc Augmentation with BMAC cells</Link></li>
-            <li><div style={{marginBottom:'10px'}}><Link href="https://www.centinelspine.com/"  target='_blank'>Cervical and lumbar disc Arthroplasty - Prodisc</Link></div>
-            <div><Link href="https://www.centinelspine.com/"  target='_blank'>Cervical and lumbar disc Arthroplasty - Simplify
-</Link></div></li>
+            <li><Link href="https://www.centinelspine.com/"  target='_blank'>Cervical and lumbar disc Arthroplasty - Prodisc</Link></li>
+            <li><Link href="https://www.centinelspine.com/"  target='_blank'>Cervical and lumbar disc Arthroplasty - Simplify</Link></li>
             <li><Link href="/Materials/surgical-guide-minimally-invasive-tubular-surgery">Surgical Guide for Minimally Invasive Tubular Surgery</Link></li>
             <li><Link href="/Materials/surgical-guide-minimally-invasive-tlif">Surgical Guide for Minimally Invasive TLIF</Link></li>
             <li><Link href="/Materials/surgical-guide-elif-xlif-llif-dlif">Surgical Guide for ELIF, XLIF, LLIF, DLIF</Link></li>
